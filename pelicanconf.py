6c725fbaa55c000f3b7c023e509c385469bac46b
Lee Dobryden <lndobryden@gmail.com>
2017-07-25 10:08:50 -0700

    Put google analytics var in the right config file, oops

--- pelicanconf.py
+++ pelicanconf.py
@@ -47,8 +47,6 @@ PHOTO_WATERMARK_TEXT = 'Lee Dobryden'
 #Theme Vars
 USE_LESS = True
 
-GOOGLE_ANALYTICS = "UA-103198821-1"
-
 STATIC_PATHS = ['images',
     'static/keybase.txt',
     'static/.well-known'
